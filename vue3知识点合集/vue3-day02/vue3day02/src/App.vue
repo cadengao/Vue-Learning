2600e42eca9b62b79693909a84b38b531f14680a
gaoxiong04 <gaoxiong04@maoyan.com>
2023-11-24 14:37:20 +0800

    课件资料：https://www.bilibili.com/video/BV1QA4y1d7xf?p=1&vd_source=bcaa6d8a3a19b9a203e13a619a137a92

---
+++ vue3知识点合集/vue3-day02/vue3day02/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import HomeView from "./components/HomeView.vue";
+export default {
+  data() {
+    return {
+      message: "helloworld",
+    };
+  },
+  components: {
+    HomeView,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <HomeView></HomeView>
+  </div>
+</template>
+
+<style>
+</style>
